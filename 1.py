1d37106532b370a6147d0939d0aabcb16f147218
nikhila-dev <67772832+nikhila-dev@users.noreply.github.com>
2020-12-07 15:02:53 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,5 @@
+import datetime 
+date=str(input('Enter the date:'))
+day_name= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+day = datetime.datetime.strptime(date, '%d %m %Y').weekday()
+print(day_name[day]) 
